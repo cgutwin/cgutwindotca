cee0a2c09e80bbe79b52bc51d18c07a13a064cbe
cgutwin <24538491+cgutwin@users.noreply.github.com>
2022-03-29 22:13:49 -0700

    build: include idea run configurations, add package-lock.json

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,30 +1,34 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.cgutwin.ca`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      // @ts-ignore
+      __key: "images"
+    }, {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      // @ts-ignore
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+  ]
+}
 
-export default config;
+export default config
